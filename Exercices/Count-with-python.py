31044d99a01029f7f5df42f08ca3c097f3c3428e
Léo Lassarade <lassarade.l@gmail.com>
2021-08-23 14:56:41 +0200

    Added better print labels to answers

--- Exercices/Count-with-python.py
+++ Exercices/Count-with-python.py
@@ -4,13 +4,13 @@ import math
 a, b = 3, 4
 c = math.sqrt(a ** 2 + b ** 2)
 
-print(c)
+print("1a : " + c)
 
 '''1b'''
 c2, a2 = 7, 5
 b2 = round(math.sqrt(c2 ** 2 - a2 ** 2), 2)
 
-print(b2)
+print("1b : " + b2)
 
 
 ''''2'''
@@ -20,4 +20,4 @@ print(b2)
 def accuracy (TP, TN, FP, FN):
     return (sumTPTN := TP + TN) / (sumTPTN + FP + FN)
 
-print(accuracy(2,985,2,11))
+print("Accuracy:" + accuracy(2,985,2,11))
