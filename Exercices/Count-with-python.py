6e8c6d0012160f25290e6b80be6c55cce5e238ac
Léo Lassarade <lassarade.l@gmail.com>
2021-08-23 14:24:45 +0200

    Simplified formulas in exercice 2, and labelled exercice 2

--- Exercices/Count-with-python.py
+++ Exercices/Count-with-python.py
@@ -6,11 +6,9 @@ c = math.sqrt(a ** 2 + b ** 2)
 
 print(c)
 
+'''2'''
 c2, a2 = 7, 5
-c2square = c2 ** 2
-a2square = a2 ** 2
-
-b2 = round(math.sqrt(c2square - a2square), 2)
+b2 = round(math.sqrt(c2 ** 2 - a2 ** 2), 2)
 
 print(b2)
 
