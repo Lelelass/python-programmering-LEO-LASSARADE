018555b0e75b42958ecdaafb7d66ca2c9e5b3e7e
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-14 08:58:15 +0200

    Update pika_reco.py

--- Labb_2/pika_reco.py
+++ Labb_2/pika_reco.py
@@ -4,20 +4,24 @@ import pandas as pd
 
 def read_and_format(csv_file):
         df = pd.read_csv(csv_file)
-        df.columns.values[0] = df.columns.values[0][1:]
-        df.columns.values[1] = df.columns.values[1][:-1]
+        df.columns.values[0] = "Width"
+        df.columns.values[1] = "Height"
         df.iloc[:, 0] = df.iloc[:, 0].str.replace('(', '')
         df.iloc[:, 1] = df.iloc[:, 1].str.replace(')', '')
+        df['Width'] = df['Width'].astype(float)
+        df['Height'] = df['Height'].astype(float)
         return df
 
 
 pikachu_data = read_and_format('pikachu.csv')
 pichu_data = read_and_format('pichu.csv')
 
+print(type(pikachu_data.iloc[0][0]))
+
 print(pikachu_data.head())
 print(pichu_data.head())
 
 
 
-plt.scatter(pika_data.iloc[:,0], pika_data.iloc[:,1])
+plt.scatter(pikachu_data.iloc[:,0], pikachu_data.iloc[:,1])
 plt.scatter(pichu_data.iloc[:,0], pichu_data.iloc[:,1])
