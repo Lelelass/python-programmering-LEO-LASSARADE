ba2fa8a71f51f09c199547dca0753af44cd1c8b3
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-16 10:37:18 +0200

    Update pika_main.py
    Updated to reflect current functional availability from pika_data

--- Labb_2/pika_main.py
+++ Labb_2/pika_main.py
@@ -4,11 +4,13 @@ import pandas as pd
 import pika_data as pida
 
 
-
 pikachu_data = pida.read_and_format('pikachu.csv')
 pichu_data = pida.read_and_format('pichu.csv')
 
-print(type(pikachu_data.iloc[0][0]))
+pikachu_training_data, pikachu_test_data = pida.sample_randomization(pikachu_data)# Functionen returnerar en tuple så jag vill komma åt första värden i tuplet
+
+print(pikachu_training_data)
+print(pikachu_test_data)
 
 print(pikachu_data.head())
 print(pichu_data.head())
