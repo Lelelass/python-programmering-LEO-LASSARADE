b6f09928f430c3a674c1e83890ede3bc37ebcb38
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-16 14:00:49 +0200

    Update pika_calc.py
    Forumlas tryouts

--- Labb_2/pika_calc.py
+++ Labb_2/pika_calc.py
@@ -1,13 +1,6 @@
-import math
-from statistics import mean
-
-def eucl_dist(P,Q):
-    distance = math.sqrt((P[0]-Q[0])**2 + (P[1]-Q[1])**2)
-    return distance
-
 
 def test_point_distance(test_point, data):
-    distances = []
-    for index, row in data.iterrows():
-        distances.append(eucl_dist(test_point,(row['Width'],row['Height'])))
-    return mean(distances) # https://www.geeksforgeeks.org/find-average-list-python/
+    data['distance']  = ((data['Width'] - test_point[0])**2 + (data['Height'] - test_point[1])**2)**0.5
+    return data
+    #return data['distance'].min()# Minimal value for column distance
+
