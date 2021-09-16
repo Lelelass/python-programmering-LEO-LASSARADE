f82afa32dcafa8aa1d42eaa00179694576747a82
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-16 10:36:28 +0200

    Update pika_data.py
    added dataframe randomization and set splitting (train, test) function

--- Labb_2/pika_data.py
+++ Labb_2/pika_data.py
@@ -9,3 +9,9 @@ def read_and_format(csv_file):
         df['Width'] = df['Width'].astype(float)# 2 rad kod för att byta strings som finns i varje kolumn (original datatyp) till float
         df['Height'] = df['Height'].astype(float)
         return df
+
+def sample_randomization(dataframe): # koden har jag fått reda på från https://www.geeksforgeeks.org/pandas-how-to-shuffle-a-dataframe-rows/
+        random_df = dataframe.sample(frac = 1)# Blandar raderna
+        train_data = random_df[0:44]
+        test_data = random_df[45:]
+        return train_data, test_data
