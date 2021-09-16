e81156fa8721ec88abe466d57e6cdd05d30a8d5c
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-16 09:41:57 +0200

    Update pika_reco.py
    Kod kommentar och instruktioner

--- Labb_2/pika_reco.py
+++ Labb_2/pika_reco.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 def read_and_format(csv_file):
         df = pd.read_csv(csv_file)
-        df.columns.values[0] = "Width"
+        df.columns.values[0] = "Width"# Manuellt n¨mning av kolumner
         df.columns.values[1] = "Height"
-        df.iloc[:, 0] = df.iloc[:, 0].str.replace(r'(', '', regex=True)
+        df.iloc[:, 0] = df.iloc[:, 0].str.replace(r'(', '', regex=True)# 2 rad kod för att erätta värden i celler med värden utan parenthes
         df.iloc[:, 1] = df.iloc[:, 1].str.replace(r')', '', regex=True)
-        df['Width'] = df['Width'].astype(float)
+        df['Width'] = df['Width'].astype(float)# 2 rad kod för att byta strings som finns i varje kolumn (original datatyp) till float
         df['Height'] = df['Height'].astype(float)
         return df
 
@@ -22,6 +22,9 @@ print(pikachu_data.head())
 print(pichu_data.head())
 
 
-
+# tittle x labels Y labels, färg + teckestil (kryss) - legend
 plt.scatter(pikachu_data.iloc[:,0], pikachu_data.iloc[:,1])
 plt.scatter(pichu_data.iloc[:,0], pichu_data.iloc[:,1])
+
+#Eucl avståndet från testpunkt till datapunkt (varje punkt)
+
