31483cc8d4a59d479673f8b686a67fcf2c6671fc
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-17 11:16:18 +0200

    Update pika_main.py
    Moving towards multiple points disatnce testing instead of single minimum point.

--- Labb_2/pika_main.py
+++ Labb_2/pika_main.py
@@ -11,21 +11,39 @@ pichu_data = pida.read_and_format('pichu.csv')
 pikachu_training_data, pikachu_test_data = pida.sample_randomization(pikachu_data)# Functionen returnerar en tuple så jag vill komma åt första värden i tuplet
 pichu_training_data, pichu_test_data = pida.sample_randomization(pichu_data)
 
+
+test_point = (pikachu_test_data.iloc[0][0], pikachu_test_data.iloc[0][1])
+pikachu_training_data = picalc.test_point_distance(test_point, pikachu_training_data)# Calls function for adding a column distance to dataframe
+pichu_training_data = picalc.test_point_distance(test_point, pichu_training_data)
+
+
+pikachu_training_data = pikachu_training_data.sort_values('distance')# Sorts column distance in ascending order (default)
+pichu_training_data = pichu_training_data.sort_values('distance')
+pikachu_distances = pikachu_training_data.iloc[:5,2].tolist()
+pichu_distances = pichu_training_data.iloc[:5,2].tolist()
+
+pikachu_closer = 0
+pichu_closer = 0
+closer_matches = {}
+for distance_pika in pikachu_distances:
+    for distance_pichu in pichu_distances:
+        if distance_pika < distance_pichu:
+            continue
+        else:
+            #Add pichu object to dict
+            break
+#Add pikachu object to dict
+
+
+print(f"pika : {pikachu_closer}, pichu : {pichu_closer}")
+
+print(pikachu_distances)
+print(pichu_distances)
+
 #pikachu_training_data = picalc.test_point_distance((pikachu_test_data.iloc[0][0], pikachu_test_data.iloc[0][1]), pikachu_training_data)
-#print(pikachu_training_data)
-
-#test_point = (pikachu_test_data.iloc[0][0], pikachu_test_data.iloc[0][1])
-#print(test_point)
-
-#pikachu_training_data['distance']  = ((pikachu_training_data['Width'] - test_point[0])**2 + (pikachu_training_data['Height'] - test_point[1])**2)**0.5
-#pichu_training_data['distance']  = ((pichu_training_data['Width'] - test_point[0])**2 + (pichu_training_data['Height'] - test_point[1])**2)**0.5
-#distance_to_pikachu = pikachu_training_data['distance'].min()# Minimal value for column distance
-#distance_to_pichu = pichu_training_data['distance'].min()
-
-#print(distance_to_pikachu)
-#print(distance_to_pichu)
 
 
+'''
 while True:
     try:
         user_input = input("Two points separated by a space").split(" ")
@@ -53,8 +71,10 @@ pichu_count = 0
 
 while i < 5:
     test_point = (pichu_test_data.iloc[i][0],pichu_test_data.iloc[i][1])
-    pikachu_training_data['distance']  = ((pikachu_training_data['Width'] - test_point[0])**2 + (pikachu_training_data['Height'] - test_point[1])**2)**0.5
-    pichu_training_data['distance']  = ((pichu_training_data['Width'] - test_point[0])**2 + (pichu_training_data['Height'] - test_point[1])**2)**0.5
+    pikachu_training_data = picalc.test_point_distance(test_point, pikachu_training_data)# Calls function for adding a column distance to dataframe
+    pichu_training_data = picalc.test_point_distance(test_point, pichu_training_data)
+    pikachu_training_data.sort_values('distance')
+    pichu_training_data.sort_values('distance')
     distance_to_pikachu = pikachu_training_data['distance'].min()# Minimal value for column distance
     distance_to_pichu = pichu_training_data['distance'].min()
     if distance_to_pichu > distance_to_pikachu:
@@ -73,6 +93,3 @@ plt.scatter(pikachu_training_data.iloc[:,0], pikachu_training_data.iloc[:,1])
 plt.scatter(pichu_training_data.iloc[:,0], pichu_training_data.iloc[:,1])
 plt.scatter(pichu_test_data.iloc[:,0], pichu_test_data.iloc[:,1])
 plt.scatter(pikachu_test_data.iloc[:,0], pikachu_test_data.iloc[:,1])
-
-
-'''
