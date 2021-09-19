14a1dd93eb93afc71d43bece984c6d958af2b16c
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-19 16:06:09 +0200

    Update pika_calc.py

--- Labb_2/pika_calc.py
+++ Labb_2/pika_calc.py
@@ -2,5 +2,4 @@
 def test_point_distance(test_point, data):
     data['distance']  = ((data['Width'] - test_point[0])**2 + (data['Height'] - test_point[1])**2)**0.5
     return data
-    #return data['distance'].min()# Minimal value for column distance
 
