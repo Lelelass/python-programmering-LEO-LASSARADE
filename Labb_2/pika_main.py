535a9e0f581fc55cb119756b1524c83535f83020
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-20 13:19:17 +0200

    Update pika_main.py
    Improved file description by comments

--- Labb_2/pika_main.py
+++ Labb_2/pika_main.py
@@ -4,16 +4,28 @@ import pandas as pd
 import pika_data as pida
 import pika_calc as picalc
 
-
+'''
+The function read_and_format cleans the csv file (previously created by txt_to_csv.py) from parenthesis,
+transforms it's values to float and store them in a dataframe with Height and Width columns
+'''
 pikachu_data = pida.read_and_format('pikachu.csv')
 pichu_data = pida.read_and_format('pichu.csv')
 
-pikachu_training_data, pikachu_test_data = pida.sample_randomization(pikachu_data)# The function returns two dataframess as a tuple, I store each dataframe in an appropriate variable for test and train data
+'''
+The function sample_randomization shuffles the index of the imported dataframes so the data is randomized.
+it then slices the data in proportion 95 - 5 and returns two dataframes as a tuple.
+I store each dataframe in an appropriate variable for test and for train data.
+'''
+pikachu_training_data, pikachu_test_data = pida.sample_randomization(pikachu_data)
 pichu_training_data, pichu_test_data = pida.sample_randomization(pichu_data)
 
-test_points = pida.test_df_to_coordinates_list(pikachu_test_data, pichu_test_data)# List of the ten test points' coordinates
+'''
+List of the ten test points' coordinates, generated through function passed on test data. (5 from pichu file and 5 from pikachu file).
+Those are the points that will be tested in the following loop
+'''
+test_points = pida.test_df_to_coordinates_list(pikachu_test_data, pichu_test_data)
 
-pikachu_classified = 0
+pikachu_classified = 0 #initialising values for how points are classified by following algorithm
 pichu_classified = 0
 
 for test_point in test_points : #each test point will be tested against it's 5 closest points in both datasets.
@@ -22,6 +34,8 @@ for test_point in test_points : #each test point will be tested against it's 5 c
 
     pikachu_distances = pida.sort_and_list(pikachu_training_data) #This sorts the dataframe with distance to point ascending, and returns a list of the 5 smallest values. (5 closest points to test value)
     pichu_distances = pida.sort_and_list(pichu_training_data)
+    print(pikachu_distances)
+    print(pichu_distances)
    
     closer_matches = picalc.list_comparison(pikachu_distances,pichu_distances)
 
@@ -35,9 +49,6 @@ for test_point in test_points : #each test point will be tested against it's 5 c
 
 print(f"test points were classified as {pikachu_classified} pikachus and {pichu_classified} pichus. Expected 5 pikachus and 5 pichus")
 
-
-
-    #pikachu_training_data = picalc.test_point_distance((pikachu_test_data.iloc[0][0], pikachu_test_data.iloc[0][1]), pikachu_training_data)
 
 '''
     i = 0
