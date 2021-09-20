9878b0d66ca652b95244ce6d7b9ee966ecdf3864
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-09-20 13:40:08 +0200

    Update pika_main.py
    Added functionality for user input and further commenting

--- Labb_2/pika_main.py
+++ Labb_2/pika_main.py
@@ -20,9 +20,35 @@ pikachu_training_data, pikachu_test_data = pida.sample_randomization(pikachu_dat
 pichu_training_data, pichu_test_data = pida.sample_randomization(pichu_data)
 
 '''
+Here starts the part of the exercice where a single test point is provided by an user
+'''
+
+try:
+    user_input = input("Give a point").split(" ")
+    user_input_point = [float(input) for input in user_input]
+    print(f"You gave : {user_input_point}")
+    #The following series of methods will be detailed in the second part of the exercice
+    pikachu_training_data = picalc.test_point_distance(user_input_point, pikachu_training_data)
+    pichu_training_data = picalc.test_point_distance(user_input_point, pichu_training_data)
+    pikachu_distances = pida.sort_and_list(pikachu_training_data)
+    pichu_distances = pida.sort_and_list(pichu_training_data)
+    closer_matches = picalc.list_comparison(pikachu_distances,pichu_distances)
+
+    if len(closer_matches['pikachu']) > len(closer_matches['pichu']):
+        print(f"Test point is likely a pikachu ({len(closer_matches['pikachu'])} closer pikachus, {len(closer_matches['pichu'])} closer pichus)")
+    else:
+        print(f"Test point is likely a pichu ({len(closer_matches['pikachu'])} closer pikachus, {len(closer_matches['pichu'])} closer pichus)")
+    
+except ValueError as err:
+    print(err)
+
+
+
+'''
 List of the ten test points' coordinates, generated through function passed on test data. (5 from pichu file and 5 from pikachu file).
 Those are the points that will be tested in the following loop
 '''
+
 test_points = pida.test_df_to_coordinates_list(pikachu_test_data, pichu_test_data)
 
 pikachu_classified = 0 #initialising values for how points are classified by following algorithm
@@ -31,17 +57,15 @@ pichu_classified = 0
 for test_point in test_points : #each test point will be tested against it's 5 closest points in both datasets.
     pikachu_training_data = picalc.test_point_distance(test_point, pikachu_training_data)# Calls function for adding a column distance to dataframe
     pichu_training_data = picalc.test_point_distance(test_point, pichu_training_data)
-
     pikachu_distances = pida.sort_and_list(pikachu_training_data) #This sorts the dataframe with distance to point ascending, and returns a list of the 5 smallest values. (5 closest points to test value)
     pichu_distances = pida.sort_and_list(pichu_training_data)
-    print(pikachu_distances)
-    print(pichu_distances)
+
    
-    closer_matches = picalc.list_comparison(pikachu_distances,pichu_distances)
+    closer_matches = picalc.list_comparison(pikachu_distances,pichu_distances) # This as been the hardet of the exercice, comparing two lists, and extracting the minimal values to a correctly labelled dictionnary.
 
 
     if len(closer_matches['pikachu']) > len(closer_matches['pichu']):
-        pikachu_classified += 1
+        pikachu_classified += 1 #As the code is ran 10 times, I wanted to store all the outputs.
         print(f"Test point is likely a pikachu ({len(closer_matches['pikachu'])} closer pikachus, {len(closer_matches['pichu'])} closer pichus)")
     else:
         pichu_classified += 1
