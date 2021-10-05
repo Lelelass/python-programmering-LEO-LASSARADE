7b2ab8bb3fc66e643b30ee0dbfb3abb36937e286
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-10-05 14:29:21 +0200

    Update test_geometry.py
    Tests for Circle and Cube classes added

--- Labb_3/test_geometry.py
+++ Labb_3/test_geometry.py
@@ -1,7 +1,7 @@
 import unittest
 from numpy import pi
-from geometry import Circle
-from geometry import Rectangle
+from geometry import Circle, Rectangle, Cube, Sphere
+
 
 class TestRectangle(unittest.TestCase):
     def setUp(self) -> None:
@@ -40,9 +40,9 @@ class TestRectangle(unittest.TestCase):
 class TestCircle(unittest.TestCase):
     def setUp(self) -> None:
         self.center = (0,0,0)
-        self.radius = 4
+        self.radius = 1
     
-    def create_circle(self):
+    def create_circle(self) -> "Circle":
         return Circle(self.center, self.radius)
 
     def test_radius(self):
@@ -50,6 +50,37 @@ class TestCircle(unittest.TestCase):
             circle = Circle((0,0,0), -1)
         with self.assertRaises(TypeError):
             circle = Circle((0,0,0), "one")
+
+    def test_area(self):
+        circle1 = self.create_circle()
+        self.assertAlmostEqual(circle1.area(), pi)
+        circle2 = Circle((0,0,0),2)
+        self.assertAlmostEqual(circle2.area(), 4 * pi)
+    
+    def test_adding_circles(self):
+        circle1 = self.create_circle()
+        circle2 = Circle((0,0,0),2)
+        self.assertEqual(circle1 + circle2, Circle((0,0,0),3))
+    
+    def test_adding_circle_with_rect(self):# Usefull test, had forgotten error raise on Circle __add__ . Nothing happened, test did not finish.
+        circle1 = self.create_circle()
+        rect1 = Rectangle((1,1,1),2,3)
+        with self.assertRaises(TypeError):
+            no_shape = circle1 + rect1
+
+class TestCube(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.center = (0,0,0)
+        self.lenght = 3
+    
+    def create_cube(self)-> "Cube":
+        return Cube(self.center, self.lenght)
+
+    def test_cube_translate(self):
+        pass
+
+    
 
 if __name__ == "__main__":
     unittest.main()
